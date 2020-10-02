8549b31f899332a435f7837a82b8f9affb04aa66
Aiden-Qian <69572923+Aiden-Qian@users.noreply.github.com>
2020-10-02 23:16:57 +0800

    Create DeleteNodes.java

---
+++ Code_Lab/Week2/DeleteNodes.java
@@ -0,0 +1,22 @@
+public Class DeleteNodes{
+  public ListNode deleteNodes(ListNode head, int m, int n) {
+        ListNode pointer = new ListNode(0);
+        pointer.next = head;
+        while (pointer != null) {
+            pointer = getPost(pointer, m);
+            if (pointer == null) {
+                break;
+            }
+            ListNode postNode = getPost(pointer, n + 1);
+            pointer.next = postNode;
+        }
+        return head;
+    }
+
+    private ListNode getPost(ListNode node, int count) {
+        for (int i = 0; i < count && node != null; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+}
