e9bcf147b71570e83c09d670e104680197e65bd4
Aiden-Qian <69572923+Aiden-Qian@users.noreply.github.com>
2020-10-02 23:08:57 +0800

    Create OddEvenList.java

---
+++ Code_Lab/Week2/OddEvenList.java
@@ -0,0 +1,29 @@
+public class OddEvenList {
+    public ListNode oddevenlist(ListNode head) {
+        int i = 1;
+        ListNode cur = null, oddHead = head, evenHead = null, oddCur = oddHead, evenCur = null;
+        if(oddHead == null)
+        	return head;
+        else {
+        	evenHead = oddHead.next;
+        	evenCur = evenHead;
+        }
+        if(evenHead == null)
+        	return head;
+        cur = evenCur.next;	
+        while(cur != null){
+        	if(i % 2 == 1){
+        		oddCur.next = cur;
+        		oddCur = cur;
+        		cur = cur.next;
+        	} else {
+        		evenCur.next = cur;
+        		evenCur = cur;
+        		cur = cur.next;
+        	}
+        	i++;
+        }
+        evenCur.next = null;
+        oddCur.next = evenHead;
+        return oddHead;
+    }
