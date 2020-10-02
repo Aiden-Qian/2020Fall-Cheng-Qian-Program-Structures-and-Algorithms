4822ad1dd9892e71385b7dd4f3a6d6d94cfbad5e
Aiden-Qian <69572923+Aiden-Qian@users.noreply.github.com>
2020-10-02 23:40:09 +0800

    Create Lee24.java

---
+++ Code_Lab/Week2/Lee24.java
@@ -0,0 +1,16 @@
+public class Lee24{
+  public ListNode swapPairs(ListNode head)
+	{
+		if (head == null)
+			return null;
+ 
+		if (head.next == null)
+			return head;
+ 
+		ListNode temp = head.next;
+		head.next = swapPairs(temp.next);
+		temp.next = head;
+ 
+		return temp;
+	}
+}
